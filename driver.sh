c2e72cdd0d114ef0e62be054f78a96898ce16b77
DaltonGorham <daltongorham@gmail.com>
2025-04-19 19:37:57 -0500

    add more filtering and sorting using the power of awk

--- driver.sh
+++ driver.sh
@@ -37,12 +37,30 @@ echo "Removing header and converting all text to lowercase in ${transaction_file
 # remove the header from the transaction_file and convert all text to lowercase
 sed '1d' "$transaction_file" | tr '[A-Z]' '[a-z]' > cleaned_"$transaction_file"
 check_exit_status "Failed to reformat ${transaction_file}."
-echo "Removed header and translated all text to lowercase for ${transaction_file} successfully."
+echo "Successfully removed header and translated all text to lowercase for ${transaction_file}."
 echo "Created new formatted file: cleaned_${transaction_file}"
 
+# use the cleaned file without the header and lowercase letters for the rest of the script
 cleaned_transaction_file="cleaned_${transaction_file}"
 
 # convert the "gender" field to use "f" and "m" or "u"
 echo "Converting gender field in ${cleaned_transaction_file}..."
 convert_gender_field "${cleaned_transaction_file}" 5
 check_exit_status "Failed to convert the gender field in ${cleaned_transaction_file}."
+
+# place all records that do not contain a state in exceptions.csv and remove them from the cleaned file
+echo "Filtering records that do not contain a state in ${cleaned_transaction_file}..."
+echo "Creating exceptions.csv file..."
+filter_transaction_file "${cleaned_transaction_file}" 12
+
+echo "Removing the $ sign from the transaction amount field in ${cleaned_transaction_file}..."
+remove_dollar_sign "${cleaned_transaction_file}" 6
+
+# sort the cleaned file by customer ID and place it in the final transaction.csv file
+echo "Sorting ${cleaned_transaction_file} by customer ID..."
+sort -n -k 1,1 "${cleaned_transaction_file}" > "transaction.csv"
+check_exit_status "Failed to sort ${cleaned_transaction_file}."
+echo "Successfully sorted ${cleaned_transaction_file} by customer ID."
+echo "Removing ${cleaned_transaction_file} and placing sorted records in transaction.csv..."
+rm "${cleaned_transaction_file}"
+
--- util_functions.sh
+++ util_functions.sh
@@ -90,6 +90,103 @@ function convert_gender_field() {
     fi
 }
 
+function filter_transaction_file() {
+    local input_file=$1
+    local filter_field=$2 
+    local exceptions_file="exceptions.csv"
+
+    # create a temp file to store the output and avoid overwriting the original file if it fails
+    local tmp_file="$(mktemp)"
+
+    # use -v to pass the field number and exceptions_file to awk because awk does not use bash variables
+    # use -F to set the field separator to comma
+    awk -v state_field="$filter_field" -v exceptions="$exceptions_file" -F ',' '
+
+    {
+        # set output field separator to comma
+        OFS = ","
+
+          # if the field number is greater than the number of fields, print an error message
+        if (state_field > NF) {
+            print "Error: Field number exceeds the number of fields in the file." > "/dev/stderr" # use awk syntax to print to stderr
+            exit 1
+        }
+        else if (state_field < 1 ) {
+            print "Error: Field number must be greater than 0." > "/dev/stderr" # use awk syntax to print to stderr
+            exit 1
+        }
+
+        # set state to the current value of the field
+        state = $state_field
+
+        if (! state || state == "NA") {
+            # print these lines to the exceptions file
+            print > exceptions
+            # skip to the next line to avoid keeping them in correct output
+            next
+        }
+
+        print 
+    }' "$input_file" > "$tmp_file"
+
+    if [ $? -eq 0 ]; then
+        # replace the original file with the temp file that has the changes
+        mv "$tmp_file" "$input_file"
+        echo "Successfully filtered $input_file."
+    else
+        echo "Error: Failed to filter $input_file." >&2
+        rm "$tmp_file"
+        exit 1
+    fi
+}
+
+function remove_dollar_sign() {
+    local input_file=$1
+    local field_number=$2
+
+    # create a temp file to store the output and avoid overwriting the original file if it fails
+    local tmp_file="$(mktemp)"
+
+    # use -v to pass the field number to awk because awk does not use bash variables
+    # use -F to set the field separator to comma
+    awk -v field="$field_number" -F ',' '
+    {
+        # set output field separator to comma
+        OFS = ","
+
+        # if the field number is greater than the number of fields, print an error message
+        if (field > NF) {
+            print "Error: Field number exceeds the number of fields in the file." > "/dev/stderr" # use awk syntax to print to stderr
+            exit 1
+        }
+        else if (field < 1 ) {
+            print "Error: Field number must be greater than 0." > "/dev/stderr" # use awk syntax to print to stderr
+            exit 1
+        }
+
+        # set the field to the current value of the field
+        purchase_amount = $field
+
+        # remove the dollar sign from the purchase amount using substr
+        # if the purchase amount starts with a dollar sign then remove by taking the substring starting from the second character
+        if (purchase_amount ~ /^\$/) {
+           $field = substr(purchase_amount, 2)
+        }
+
+        print 
+    }' "$input_file" > "$tmp_file"
+
+    if [ $? -eq 0 ]; then
+        # replace the original file with the temp file that has the changes
+        mv "$tmp_file" "$input_file"
+        echo "Successfully removed dollar sign from $input_file."
+    else
+        echo "Error: Failed to remove dollar sign from $input_file." >&2
+        rm "$tmp_file"
+        exit 1
+    fi
+}
+
 function get_os() {
     local os=$(uname)
     echo "$os"
